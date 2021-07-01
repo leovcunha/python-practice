97f3da8cf72ff8edbe5991beec416f49cc4d4e9c
leovcunha <lvcunha@gmail.com>
2021-07-01 13:06:58 -0300

    Update roman-numerals.py

--- roman-numerals.py
+++ roman-numerals.py
@@ -9,9 +9,9 @@ d5 = ["V", "L", "D", ""]
 d1 = ["I","X", "C" ,"M"]
 
 for i in range(n):
-    for j in range(len(arr[i])-1,-1,-1):
+    for j in range(len(arr[i])-1,-1,-1): #pick digits from ascending place value (ex: 152 = 2 -> 5 -> 1)
         digit = int(arr[i][j])
-        p = len(arr[i])-1-j
+        p = len(arr[i])-1-j #place value: 0=units, 1=tens, 2=hundreds, 3=thousands
         if digit == 9:
             r = d1[p]+d1[p+1]
         elif digit > 4 and digit < 9:
